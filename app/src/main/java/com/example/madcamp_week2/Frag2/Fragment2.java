856dbcf99b28c2690db1a400151d77b1b3044eab
Jaegoomon <jaegoo1199@gmail.com>
2021-01-10 23:52:29 +0900

    permission gradle setting

--- app/src/main/java/com/example/madcamp_week2/Frag2/Fragment2.java
+++ app/src/main/java/com/example/madcamp_week2/Frag2/Fragment2.java
@@ -10,58 +10,30 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.madcamp_week2.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment2#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Fragment2 extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FloatingActionButton load;
+    private FloatingActionButton upload;
+    private FloatingActionButton serverLoad;
+    private FloatingActionButton multiLoad;
 
     public Fragment2() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment2.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment2 newInstance(String param1, String param2) {
-        Fragment2 fragment = new Fragment2();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        load = (FloatingActionButton) findViewById(R.id.fab);
+        upload = (FloatingActionButton) findViewById(R.id.fabUpload);
+        serverLoad = (FloatingActionButton) findViewById(R.id.fabLoad);
+        multiLoad = (FloatingActionButton) findViewById(R.id.fabMulti);
+        imageView = findViewById(R.id.imageView);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_2, container, false);
     }
 }
--- app/src/main/java/com/example/madcamp_week2/MainActivity.java
+++ app/src/main/java/com/example/madcamp_week2/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.madcamp_week2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.example.madcamp_week2.Activity.ViewpagerAdapter;
@@ -13,10 +17,23 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISION_REQUEST = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if ((ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) ||
+                (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.READ_CONTACTS},
+                    MY_PERMISION_REQUEST);
+        }
 
         ViewPager vp = findViewById(R.id.viewpager);
         ViewpagerAdapter adapter = new ViewpagerAdapter(getSupportFragmentManager());
@@ -30,6 +47,6 @@ public class MainActivity extends AppCompatActivity {
         images.add(R.drawable.photos);
         images.add(R.drawable.health);
 
-        for(int i=0;i<3;i++) tab.getTabAt(i).setIcon(images.get(i));
+        for (int i = 0; i < 3; i++) tab.getTabAt(i).setIcon(images.get(i));
     }
 }
