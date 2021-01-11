eee34203daf6db4c7388f383b1887a6f8371a8b0
hyuna <kiipo0623@gmail.com>
2021-01-11 17:19:26 +0900

    facebook

---
+++ app/src/main/java/com/example/madcamp_week2/Activity/SplashActivity.java
@@ -0,0 +1,98 @@
+package com.example.madcamp_week2.Activity;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.madcamp_week2.MainActivity;
+import com.example.madcamp_week2.R;
+import com.facebook.login.Login;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        setContentView(R.layout.activity_splash);
+
+        callbackManager = CallbackManager.Factory.create();
+
+//        Handler handler = new Handler();
+//        handler.postDelayed(new splashhandler(), 2000);  // 3000Millis = 3초, 로딩 시간
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        startActivity(new Intent(getApplication(), MainActivity.class)); // 로딩 끝나고, 메인으로
+                        finish();
+                        Log.v("result",object.toString());
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                graphRequest.setParameters(parameters);
+                graphRequest.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e("LoginErr",error.toString());
+            }
+        });
+
+
+
+    }
+/*
+    private class splashhandler implements Runnable{
+        @Override
+        public void run() {
+            startActivity(new Intent(getApplication(), MainActivity.class)); // 로딩 끝나고, 메인으로
+            finish();
+        }
+    }
+*/
+    @Override
+    public void onBackPressed() {
+        // 뒤로 가기 못하게
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
